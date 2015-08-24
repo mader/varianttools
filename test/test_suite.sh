17e2b5a7e1131f9b86fa0a52307426ab3ed4ab90
Malte Mader <malte.mader@ti.bund.de>
2015-08-24 13:10:16 +0200

    Make testsuite run even if ruby is not in path.

--- test/test_suite.sh
+++ test/test_suite.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-../bin/varianttools.rb -f ../data/test.fasta -i ../data/snp_test1/ -t SNP -d 9,5
+ruby ../bin/varianttools.rb -f ../data/test.fasta -i ../data/snp_test1/ -t SNP -d 9,5
 diff output.tsv ../data/expected_results/snp_test1_clc_results.csv
-../bin/varianttools.rb -f ../data/test2.fasta -i ../data/snp_test2/ -t SNP -d 9,5
+ruby ../bin/varianttools.rb -f ../data/test2.fasta -i ../data/snp_test2/ -t SNP -d 9,5
 diff output.tsv ../data/expected_results/snp_test2_clc_results.csv
-../bin/varianttools.rb -f ../data/test.fasta -i ../data/indel_test1/ -t INDEL -d 9,5
+ruby ../bin/varianttools.rb -f ../data/test.fasta -i ../data/indel_test1/ -t INDEL -d 9,5
 diff output.tsv ../data/expected_results/indel_test1_clc_results.csv
 rm logfile.log
 rm output.tsv
