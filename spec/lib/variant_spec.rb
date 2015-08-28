204cb798410c14946d4f5c89026291f66e2fed25
Malte Mader <malte.mader@ti.bund.de>
2015-08-28 12:32:55 +0200

    Add file header in spec files.

--- spec/lib/variant_spec.rb
+++ spec/lib/variant_spec.rb
@@ -1,3 +1,21 @@
+#!/usr/bin/env ruby
+=begin
+  Copyright (c) 2014-2015 Malte Mader <malte.mader@ti.bund.de>
+  Copyright (c) 2014-2015 Thünen Institute of Forest Genetics
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+  
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+=end
+
 require 'spec_helper'
 
   describe Variant do
